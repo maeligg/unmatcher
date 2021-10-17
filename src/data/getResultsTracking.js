6206f93f4ab0433741a56b2dc66a7862153ce1ff
Maël Brunet <maelbrunet@gmail.com>
2021-10-17 16:18:32 +0200

    Update to google sheets API v4

--- src/data/getResultsTracking.js
+++ src/data/getResultsTracking.js
@@ -1,21 +1,23 @@
 import characters from './characters';
 
 const getResults = async () => {
-    const response = await fetch('https://spreadsheets.google.com/feeds/cells/1BOdYOeTDW0ddVBq4Oq_CWADMzIonOcBjvDRpWHuUs50/1/public/full?alt=json');
+    const response = await fetch('https://sheets.googleapis.com/v4/spreadsheets/1BOdYOeTDW0ddVBq4Oq_CWADMzIonOcBjvDRpWHuUs50/values/C2:AB27?key=AIzaSyA7apN4I6sW4kaUovxaWyvZqRu2Gp_zEPc');
     const json = await response.json();
-    const results = json.feed.entry.filter(cell => cell.gs$cell.row !== '1');
+
+    // Removing duplicate Buffy results
+    let results = json.values;
+    results = results.filter((_, index) => index !== 15 && index !== 16);
+    results = results.map(row => row.filter((_, index) => index !== 15 && index !== 16));
 
     let charactersWithResults = {};
 
-    characters.forEach(character => {
-        const indexStartOfResults = results.findIndex(cell => cell.content.$t === character.name) + 1;
-        let resultsForCharacter = results.slice(indexStartOfResults, indexStartOfResults + characters.length + 2).map(cell => parseInt(cell.content.$t));
-        resultsForCharacter.splice(15, 2); // Removing duplicate Buffy results
-
-        charactersWithResults[character.name] = characters.map(character => character.name).reduce((acc, curr, i) => {
-            acc[curr] = resultsForCharacter[i];
-            return acc;
-        }, {});
+    characters.forEach((firstChar, firstCharIndex) => {
+        characters.forEach((secondChar, secondCharIndex) => {
+            if (!charactersWithResults[firstChar.name]) {
+                charactersWithResults[firstChar.name] = {};
+            }
+            charactersWithResults[firstChar.name][secondChar.name] = parseInt(results[firstCharIndex][secondCharIndex]);
+        })
     });
 
     return charactersWithResults;
